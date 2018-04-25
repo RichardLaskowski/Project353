6bebb6d018505b8b8e31ea3801b23c91f8d8231b
Ricky Laskowski <Richard.lee.laskowski@gmail.com>
2018-04-25 13:19:55 -0500

    Logout implemented. Update in the works

--- src/java/Filters/LoginFilter.java
+++ src/java/Filters/LoginFilter.java
@@ -6,6 +6,7 @@
 package Filters;
 
 import Controller.LoginController;
+import com.sun.xml.rpc.processor.modeler.j2ee.xml.string;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -15,6 +16,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -33,28 +35,27 @@ public class LoginFilter implements Filter
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
     {
-        System.out.println("LOGINFILTER: doFilter()");
+        //System.out.println("LOGINFILTER: doFilter()");
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse resp = (HttpServletResponse) response;
         LoginController session = (LoginController) req.getSession().getAttribute("loginController");
+        HttpSession loginSession = req.getSession();
         //System.out.println("LOGINFILTER: isLoggedIn = " + session.getIsLoggedIn());
         String url = req.getRequestURI();
         System.out.println("LOGINFILTER: requestURL = " + url);
         
         if(session == null || !session.getIsLoggedIn())
         {
-            System.out.println("LOGINFILTER: session = null isLogginIn = null");
+            //System.out.println("LOGINFILTER: session = null isLogginIn = null");
             if(url.indexOf("profile.xhtml") >= 0)
             {
-                System.out.println("LOGINFILTER: requested profile.xhtml");
+                System.out.println("LOGINFILTER: request NOT GRANTED");
+                System.out.println("LOGINFILTER: redirect - " + req.getServletContext().getContextPath() + "/logIn.xhtml");
                 resp.sendRedirect(req.getServletContext().getContextPath() + "/logIn.xhtml");
             }
-            // else if(url.indexOf("details.xhtml") >= 0 || url.indexOf("recruiterDetails.xhtml") >= 0)
-            // {
-            //     resp.sendRedirect(req.getServletContext().getContextPath() + "/signUp.xhtml");
-            // }
             else
             {
+                System.out.println("LOGINFILER: request GRANTED");
                 chain.doFilter(request, response);
             }
         }
@@ -62,15 +63,29 @@ public class LoginFilter implements Filter
         {
             if(url.indexOf("details.xhtml") >= 0 || url.indexOf("recruiterDetails.xhtml") >= 0 || url.indexOf("signUp.xhtml") >= 0)
             {
-                resp.sendRedirect(req.getServletContext().getContextPath() + "/profile.xhtml");
+                if(session.getIsLoggedIn())
+                {
+                    System.out.println("LOGINFILTER: request NOT GRANTED");
+                    System.out.println("LOGINFILTER: redirect - " + req.getServletContext().getContextPath() + "/profile.xhtml");
+                    resp.sendRedirect(req.getServletContext().getContextPath() + "/profile.xhtml");
+                }
+                else
+                {
+                    System.out.println("LOGINFILTER: request NOT GRANTED");
+                    System.out.println("LOGINFILTER: redirect - " + req.getServletContext().getContextPath() + "/logIn.xhtml");
+                    resp.sendRedirect(req.getServletContext().getContextPath() + "/logIn.xhtml");
+                }
+                
             }
             else if(url.indexOf("logOut.xhtml") >= 0)
             {
-                req.getSession().removeAttribute("LoginController");
+                System.out.println("LOGINFILER: request GRANTED");
+                loginSession.invalidate();  
                 resp.sendRedirect(req.getServletContext().getContextPath() + "/logIn.xhtml");
             }
             else
             {
+                System.out.println("LOGINFILER: request GRANTED");
                 chain.doFilter(request, response);
             }
         }
